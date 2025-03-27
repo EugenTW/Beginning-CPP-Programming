2d83a16a475665fd779032627d767f7b8931916f
eugen.lin <eugen.lin@raritan.com>
2025-03-27 16:39:49 +0800

    2025Mar27

---
+++ cmake-build-debug/S4L028/CMakeFiles/S4L028.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/S4L028.dir/main.cpp.obj"
+  "CMakeFiles/S4L028.dir/main.cpp.obj.d"
+  "S4L028.exe"
+  "S4L028.exe.manifest"
+  "S4L028.pdb"
+  "libS4L028.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/S4L028.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/S4L028/CMakeFiles/S4L028.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for S4L028.
+# This may be replaced when dependencies are built.
